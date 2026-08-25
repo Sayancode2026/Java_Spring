97f6385bb3f258fdf9a2c6b3286111bf091ef977
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-08-26 01:32:22 +0530

    Reapply "Merge remote main with BuildSSystem3"
    This reverts commit 05bac6895f3340c1b1987a61b3bd389ea8be8b6a.

---
+++ bazel_project/src/main/java/com/algo/project/utils/MyUtil.java
@@ -0,0 +1,7 @@
+package main.java.com.algo.project.utils;
+
+public class MyUtil{
+    public static void getString() {
+        System.out.println("Some String!!!!");
+    }
+}
---
+++ src/OOPS/BycycleClient.java
@@ -0,0 +1,9 @@
+package OOPS;
+
+public class BycycleClient {
+    public static void main(String[] args) {
+        Bycycle sportBicycle=new Bycycle();
+        System.out.println(sportBicycle.gear);
+        sportBicycle.braking();
+    }
+}
