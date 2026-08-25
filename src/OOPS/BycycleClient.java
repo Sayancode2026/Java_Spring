05bac6895f3340c1b1987a61b3bd389ea8be8b6a
Sayancode2026 <sayanbardhan2002@gmail.com>
2026-08-26 01:30:55 +0530

    Revert "Merge remote main with BuildSSystem3"
    This reverts commit 27410b70a4eb9b8576cb12c58da8ec9b0d73eda7, reversing
    changes made to 11cb5515a6ba8bdad5dc59aaece615bf2d933e21.

--- src/OOPS/BycycleClient.java
+++
@@ -1,9 +0,0 @@
-package OOPS;
-
-public class BycycleClient {
-    public static void main(String[] args) {
-        Bycycle sportBicycle=new Bycycle();
-        System.out.println(sportBicycle.gear);
-        sportBicycle.braking();
-    }
-}
--- src/OOPS/Contscall.java
+++
@@ -1,9 +0,0 @@
-package OOPS;
-
-public class Contscall {
-
-    public static void main(String[] args) {
-        Main2 obj=new Main2();
-        obj.getName();
-    }
-}
